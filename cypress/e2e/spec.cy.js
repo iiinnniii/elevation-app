a33fce6ac0f5360803eb8839c3b32eb8676a8c7f
Gernot Pokorny <pokorny.gernot@gmail.com>
2025-01-04 04:41:01 +0000

    #1 Added e2e-test-job

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,37 @@
-describe('template spec', function () {
-	it('passes', function () {
-		cy.visit('http://localhost:5173/');
+import dotenv from 'dotenv';
+
+dotenv.config({ path: './.env.e2e-test.local' });
+dotenv.config({ path: './.env.e2e-test.ci' });
+
+console.log('process.env.REMOTE_URL', process.env.REMOTE_URL);
+
+const remoteURL = `http://${process.env.REMOTE_URL}:5173/`; // "localhost" | "host.docker.internal" | "elevation-app-server"
+
+describe('Map Click Elevation Test', function () {
+	beforeEach(function () {
+		cy.visit(remoteURL);
+	});
+
+	it('should update elevation when clicking on the map', function () {
+		// Wait for the elevation data to be fetched and displayed
+		cy.intercept('/api/v1/test-dataset*').as('fetchElevation');
+		// cy.wait('@fetchElevation');
+
+		// Click on the map at specific coordinates
+		cy.get('.leaflet-container')
+			.click(300, 200); // Adjust coordinates as needed
+
+		cy.log('Waiting for fetchElevation request');
+		cy.wait('@fetchElevation', { timeout: 10000 }).then((interception) => {
+			cy.log('Request intercepted: ', interception);
+		});
+
+		// Assert that the elevation text is displayed and not loading
+		cy.contains('Elevation:')
+			.should('not.contain', 'Loading...')
+			.and('contain', 'meters');
+
+		// Log: Final check 
+		cy.get('p').then((p) => { cy.log('Paragraph text after fetch:', p.text()); });
 	});
 });
